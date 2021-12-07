aec2b580bc4b0865e660ba1df2ffebfb4bd28057
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-06 22:10:40 -0300

    Melhorando código e adicionando novo teste.

--- dominio.py
+++ dominio.py
@@ -26,13 +26,16 @@ class Leilao:
         self.__lances = []
 
     def propoe(self, lance: Lance):
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario:
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
             if lance.valor > self.maior_lance:
                 self.maior_lance = lance.valor
             if lance.valor < self.menor_lance:
                 self.menor_lance = lance.valor
                 
             self.__lances.append(lance)
+            
+        else:
+            raise ValueError('Erro ao propor lance.')
 
     @property
     def lances(self):
--- test_leilao.py
+++ test_leilao.py
@@ -2,58 +2,53 @@ import unittest
 from unittest import TestCase
 from dominio import Lance, Usuario, Leilao
 
-
 class TestLeilao(TestCase):
-    
+
+    # test_quando_adicionados_em_ordem_crescente_deve_retornar_o_maior_e_o_menor_valor_de_um_lance
+
     def setUp(self):
-        self.edyane = Usuario("Edyane")
-        self.lance_edyane = Lance(self.edyane, 100.0)
-        self.leilao = Leilao("Computador")
+        self.edyane = Usuario('Edyane')
+        self.lance_da_edyane = Lance(self.edyane, 150.0)
+        self.leilao = Leilao('PC')
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        lucas = Usuario("Lucas")
-        self.lance_lucas =  Lance(lucas, 150.0)
+        lucas = Usuario('Lucas')
+        lance_do_lucas = Lance(lucas, 100.0)
 
-        self.leilao.propoe(self.lance_edyane)
-        self.leilao.propoe(self.lance_lucas)
-
-        menor_valor_esperado = 100.0
-        maior_valor_esperado = 150.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
-
-    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        lucas = Usuario("Lucas")
-        self.lance_lucas =  Lance(lucas, 150.0)
-
-        self.leilao.propoe(self.lance_lucas)
-        self.leilao.propoe(self.lance_edyane)
+        self.leilao.propoe(lance_do_lucas)
+        self.leilao.propoe(self.lance_da_edyane)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
-    
+
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
+
+        with self.assertRaises(ValueError):
+            lucas = Usuario('Lucas')
+            lance_do_lucas = Lance(lucas, 100.0)
+
+            self.leilao.propoe(self.lance_da_edyane)
+            self.leilao.propoe(lance_do_lucas)
+
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
-        self.leilao.propoe(self.lance_edyane)
+        self.leilao.propoe(self.lance_da_edyane)
 
-        menor_valor_esperado = maior_valor_esperado = 100.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+        self.assertEqual(150.0, self.leilao.menor_lance)
+        self.assertEqual(150.0, self.leilao.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
-        klovis = Usuario("Klovis")
-        lucas = Usuario("Lucas")
+        lucas = Usuario('Lucas')
+        lance_do_lucas = Lance(lucas, 100.0)
+        klovis = Usuario('Klovis')
 
-        lance_klovis = Lance(klovis, 200.0)
-        self.lance_lucas =  Lance(lucas, 150.0)
+        lance_do_klovis = Lance(klovis, 200.0)
 
-        self.leilao.propoe(self.lance_lucas)
-        self.leilao.propoe(self.lance_edyane)
-        self.leilao.propoe(lance_klovis)
+        self.leilao.propoe(lance_do_lucas)
+        self.leilao.propoe(self.lance_da_edyane)
+        self.leilao.propoe(lance_do_klovis)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 200.0
@@ -61,33 +56,31 @@ class TestLeilao(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_permitir_propor_um_lance_caso_o_leilao_não_tenha_lances(self):
-        self.leilao.propoe(self.lance_edyane)
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self):
+        self.leilao.propoe(self.lance_da_edyane)
 
         quantidade_de_lances_recebido = len(self.leilao.lances)
         self.assertEqual(1, quantidade_de_lances_recebido)
 
     def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
-        lucas = Usuario("Lucas")
-        lance_lucas = Lance (lucas, 200.0)
-        
-        self.leilao.propoe(self.lance_edyane)
-        self.leilao.propoe(lance_lucas)
+        lucas = Usuario('Lucas')
+        lance_do_lucas = Lance(lucas, 200.0)
+
+        self.leilao.propoe(self.lance_da_edyane)
+        self.leilao.propoe(lance_do_lucas)
 
         quantidade_de_lances_recebido = len(self.leilao.lances)
+
         self.assertEqual(2, quantidade_de_lances_recebido)
 
-    def test_nao_deve_permitir_propor_lance_se_o_usuario_for_o_mesmo(self):
-        lance_edyane_02 = Lance(self.edyane, 345.0)
+    def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
+        lance_da_edyane_02 = Lance(self.edyane, 200.0)
 
-        self.leilao.propoe(self.lance_edyane)
-        self.leilao.propoe(lance_edyane_02)
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_da_edyane)
+            self.leilao.propoe(lance_da_edyane_02)
 
-        quantidade_de_lances_recebido = len(self.leilao.lances)
-        self.assertEqual(1, quantidade_de_lances_recebido)
 
-        
-
-#Deve importar unittest para que apareça o teste no prompt do Visual Studio Code
+# #Deve importar unittest para que apareça o teste no prompt do Visual Studio Code
 if __name__ == "__main__":
     unittest.main()
