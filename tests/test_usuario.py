19e9271606fbb48d28c5e643f8d14be30212c33d
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-08 16:27:15 -0300

    adicionando mais regras de negócio no test_usuario

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -1,9 +1,31 @@
+import pytest
 from src.leilao.dominio import Usuario, Leilao
 
-def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance():
-    lucas = Usuario('Lucas', 100.0)
-    leilao = Leilao('Carro')
+@pytest.fixture
+def lucas():
+    return Usuario('Lucas', 100.0)
 
-    lucas.propor_lance(leilao, 50)
+@pytest.fixture
+def leilao():
+    return Leilao('Café')
+
+def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(lucas, leilao):
+    lucas.propoe_lance(leilao, 50)
 
     assert lucas.carteira == 50.0
+
+def test_deve_permitir_propor_lance_menor_que_o_valor_da_carteira(lucas, leilao):
+    lucas.propoe_lance(leilao, 1.0)
+
+    assert lucas.carteira == 99.0
+
+def test_deve_permitir_propor_lance_igual_ao_valor_da_carteira(lucas, leilao):
+    lucas.propoe_lance(leilao, 100.0)
+
+    assert lucas.carteira == 0.0   
+
+def test_nao_deve_permitir_propor_lance_maior_que_o_valor_da_carteira(lucas, leilao):
+    with pytest.raises(ValueError):
+        lucas.propoe_lance(leilao, 200.0)
+
+        assert lucas.carteira == 100.0
